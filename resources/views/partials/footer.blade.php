006c97372d10d03ebf187bae6d41b81eba36eaae
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-08-09 15:23:07 -0600

    Fixed on responsive cover

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,12 +4,12 @@
             <div class="fl w-100 w-50-ns pa2 dn dn-m db-ns">
                 {!! $content->get('links_footer') !!}
             </div>
-            <div class="fl w-50 w-25-ns w-50-m pa2 mt5">
+            {{-- <div class="fl w-50 w-25-ns w-50-m pa2 mt5">
                 <img src="{{ config('app.url') .'/storage/'. app('voyager')->setting('logo_usaid') }}" alt="">
             </div>
             <div class="fl w-50 w-25-ns pa2 w-50-m mt5">
                 <img src="{{ config('app.url') .'/storage/'. app('voyager')->setting('logo_padf') }}" alt="">
-            </div>
+            </div> --}}
         </div>
         <div class="dt w-100 center tc">
             <ul class="dtc list">
